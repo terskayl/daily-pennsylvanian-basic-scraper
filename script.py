9a084fbfc27d473f837ab0a413ce43098efdaf58
Aaron Jiang <97208428+terskayl@users.noreply.github.com>
2025-02-25 02:32:58 -0500

    feat: changed script.py to read from the first opinion piece instead

--- script.py
+++ script.py
@@ -23,13 +23,13 @@ def scrape_data_point():
     headers = {
         "User-Agent": "cis3500-scraper"
     }
-    req = requests.get("https://www.thedp.com", headers=headers)
+    req = requests.get("https://www.thedp.com/section/opinion", headers=headers)
     loguru.logger.info(f"Request URL: {req.url}")
     loguru.logger.info(f"Request status code: {req.status_code}")
 
     if req.ok:
         soup = bs4.BeautifulSoup(req.text, "html.parser")
-        target_element = soup.find("a", class_="frontpage-link")
+        target_element = soup.find("a", class_="standard-link")
         data_point = "" if target_element is None else target_element.text
         loguru.logger.info(f"Data point: {data_point}")
         return data_point
